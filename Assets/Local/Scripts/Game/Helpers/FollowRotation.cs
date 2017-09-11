1600c31a3399d4fa7e463cb14ea9e6a8c8dabc5a
unknown <alvian.better@gmail.com>
2017-09-11 15:37:38 +0800

    Player model now follows camera rotation as VR only rotates camera

---
+++ Assets/Local/Scripts/Game/Helpers/FollowRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowRotation : MonoBehaviour {
+
+	public Transform targetTransform;
+
+	public bool followXAxis;
+	public bool followYAxis;
+	public bool followZAxis;
+
+	void Update() {
+		transform.rotation = Quaternion.Euler(
+			followXAxis ? targetTransform.eulerAngles.x : transform.eulerAngles.x, 
+			followYAxis ? targetTransform.eulerAngles.y : transform.eulerAngles.y, 
+			followZAxis ? targetTransform.eulerAngles.z : transform.eulerAngles.z);
+	}
+
+}
